a28910a8d8cba17a3daa440e11582120c5bfb77d
Alexandre Fonseca <alexandrejorgefonseca@gmail.com>
2015-11-09 10:57:32 +0300

    Better LabelledPatternEdge toString

--- src/main/java/io/arabesque/pattern/LabelledPatternEdge.java
+++ src/main/java/io/arabesque/pattern/LabelledPatternEdge.java
@@ -92,9 +92,7 @@ public class LabelledPatternEdge extends PatternEdge {
 
     @Override
     public String toString() {
-        return "LabelledPatternEdge{" +
-                "label=" + label +
-                "} " + super.toString();
+        return ("[" + getSrcPos() + "(" + getSrcLabel() + ")--(" + label + ")--" + getDestPos() + "(" + getDestLabel() + ")]");
     }
 
     @Override
